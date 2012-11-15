a26e30442037dddf0ad1d59d78c1a4cc399055ad
rascarlo <rascarlo@gmail.com>
2012-11-15 21:53:38 +0100

    build nexus, not combo

--- envsetup.sh
+++ envsetup.sh
@@ -431,7 +431,7 @@ function print_lunch_menu()
     echo
     echo "You're building on" $uname
     echo
-    echo "Lunch menu... pick a combo:"
+    echo "Lunch menu... pick a Nexus:"
 
     local i=1
     local choice
@@ -475,7 +475,7 @@ function lunch()
     if [ -z "$selection" ]
     then
         echo
-        echo "Invalid lunch combo: $answer"
+        echo "Invalid lunch Nexus: $answer"
         return 1
     fi
 
