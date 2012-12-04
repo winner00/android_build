2c1343a9fbcf7d2c450f4d3dc3ec9fc4ff6c5d62
rascarlo <rascarlo@gmail.com>
2012-12-04 12:56:15 +0100

    add mako to lazy sh

--- sh_build_dem_all.sh
+++ sh_build_dem_all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 source build/envsetup.sh
-lunch rascarlo_emulator-eng -j16 && lunch rascarlo_grouper-user && make otapackage -j16 && lunch rascarlo_maguro-user && make otapackage -j16
+lunch rascarlo_emulator-eng -j16 && lunch rascarlo_grouper-user && make otapackage -j16 && lunch rascarlo_maguro-user && make otapackage -j16 && lunch rascarlo_mako-user && make otapackage -j16
+
 
